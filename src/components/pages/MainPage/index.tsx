2a77335d5343852ff4c5af3b970546bc24f4dce7
yoo-jimin127 <dbwlals9936@naver.com>
2022-12-17 11:22:36 +0900

    add : api fetching & appBar

--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -1,8 +1,23 @@
+import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { ActivityComponentType } from '@stackflow/react';
 import React from 'react';
+import { MainPageAppBarLeft } from 'src/components/common/Stackflow';
+import { ProductInterface } from 'src/schemas/Product';
+import { getProductList } from 'src/services/product';
 
 const MainPage: ActivityComponentType = () => {
-  return <div></div>;
+  const [products, setProducts] = React.useState<ProductInterface[]>([]);
+  
+  const loadProducts = async () => {
+    const { data } = await getProductList();
+    setProducts(data);
+  };
+
+  React.useEffect(() => {
+    loadProducts();
+  }, []);
+
+  return <AppScreen appBar={{appendLeft: MainPageAppBarLeft}}><div>s</div></AppScreen>;
 };
 
 export default MainPage;
